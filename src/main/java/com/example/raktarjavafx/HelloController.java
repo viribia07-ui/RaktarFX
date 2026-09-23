c49c307a5d8e894447cdacb6c83abe735b2c7d7e
Bianka Viragh <viribia07@gmail.com>
2026-09-23 10:03:46 +0200

    Added functions to buttons

--- src/main/java/com/example/raktarjavafx/HelloController.java
+++ src/main/java/com/example/raktarjavafx/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.raktarjavafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    public Button buttonRaktar1, buttonRaktar2, buttonToRaktar1, buttonToRaktar2, buttonDeleteRaktar1, buttonDeleteRaktar2, buttonSave;
+    @FXML
+    public ListView<String> listViewRaktar1, listViewRaktar2;
+    @FXML
+    public TextField textFieldInput;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onButtonRaktar1() {
+        System.out.println("Button Raktár1");
+    }
+
+    @FXML
+    protected void onButtonRaktar2() {
+        System.out.println("Button Raktár2");
+    }
+    @FXML
+    protected void onButtonToRaktar1() {
+        System.out.println("Button toRaktár1");
+    }
+    @FXML
+    protected void onButtonToRaktar2() {
+        System.out.println("Button toRaktár2");
+    }
+    @FXML
+    protected void onButtonDelete1() {
+        System.out.println("Button Delete1");
+    }
+    @FXML
+    protected void onButtonDelete2() {
+        System.out.println("Button Delete2");
+    }
+    @FXML
+    protected void onButtonSave() {
+        System.out.println("Button Save");
     }
 }
